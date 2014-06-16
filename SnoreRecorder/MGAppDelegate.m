4b89094b5db2dfc697257c5d70d86a2b17d7ef7b
MichaeGon <lodnix@gmail.com>
2014-06-16 16:25:51 +0900

    時間関連更新

--- SnoreRecorder/MGAppDelegate.m
+++ SnoreRecorder/MGAppDelegate.m
@@ -8,6 +8,8 @@
 
 @implementation MGAppDelegate
 
+@synthesize date;
+
 // 初期化はここで行う
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
@@ -15,6 +17,12 @@
     
     // とりあえず現在時刻で初期化しておく
     terminatingDate=[NSDate date];
+    
+    // 現在時刻で表示を更新
+    [date setDateValue:terminatingDate];
+    // 現在時刻が指定できる最小の日付とする
+    [date setMinDate:terminatingDate];
+    
 }
 
 // 最小化から復帰時の挙動
@@ -25,6 +33,8 @@
     return NO;
 }
 
-- (IBAction)setDateAt:(NSDatePicker *)sender {
+- (IBAction)setDateAt:(NSDatePicker *)sender
+{
+    
 }
 @end
